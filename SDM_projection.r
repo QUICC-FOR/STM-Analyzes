bbee5933315349759b19a1d9d7d3b67e3aed7df7
SteveViss <s.vissault@yahoo.fr>
2015-02-11 17:08:28 -0500

    Add SDM proj

--- SDM_projection.r
+++ SDM_projection.r
@@ -1,124 +1,138 @@
 # Projection of the SDM
 # Date: 9th February, 2014
 
-# Remove climatic spaces outside of the SDM calibration
-stm_dat <- read.csv('../../STModel-Data/out_files/statesFourState.csv')
-str(stm_dat)
-selectedVars = c("annual_mean_temp", "pp_seasonality", "pp_warmest_quarter", "mean_diurnal_range","annual_pp", "mean_temperatre_wettest_quarter")
-stm_dat <- stm_dat[,selectedVars]
-rg_amt <- range(stm_dat[,1])
-rg_ppseas <- range(stm_dat[,2])
-rg_ppwarm <- range(stm_dat[,3])
-rg_md <- range(stm_dat[,4])
-rg_app <- range(stm_dat[,5])
-rg_mtwet <- range(stm_dat[,6])
+#SetWD
 
-# # Calib space
-spaceClim_SDM <- dcast(res_SDMClimate_grid,lon+lat ~ biovar, value.var="val")
-spaceClim_SDM[,4:5] <- spaceClim_SDM[,4:5]/10
-spaceClim_SDM <- subset(spaceClim_SDM, annual_mean_temp <= rg_amt[2] & annual_mean_temp >= rg_amt[1])
-spaceClim_SDM <- subset(spaceClim_SDM, pp_seasonality <= rg_ppseas[2] & pp_seasonality >= rg_ppseas[1])
-spaceClim_SDM <- subset(spaceClim_SDM, pp_warmest_quarter <= rg_ppwarm[2] & pp_warmest_quarter >= rg_ppwarm[1])
-spaceClim_SDM <- subset(spaceClim_SDM, mean_diurnal_range <= rg_md[2] & mean_diurnal_range >= rg_md[1])
-spaceClim_SDM <- subset(spaceClim_SDM, tot_annual_pp <= rg_app[2] & tot_annual_pp >= rg_app[1])
-spaceClim_SDM <- subset(spaceClim_SDM, mean_temp_wettest_quarter <= rg_mtwet[2] & mean_temp_wettest_quarter >= rg_mtwet[1])
+setwd("/home/steve/Documents/GitHub/STModel-Analyzes/")
 
-###### Load shapefiles
-
+###### Load librairies
 require("ggplot2")
-require("maptools")
-require("raster")
-require("rgeos")
 require("RColorBrewer")
 require("randomForest")
 require("nnet")
 require("reshape2")
+require("rgdal")
+require("raster")
 
-load("../data/shp_lakes_regions_study_area.Robj")
+# load SDM and shapefiles
+load('./STModel-Calibration/data/RandomForest_complete.rObj')
+load('./STModel-Calibration/data/Multinom_complete.rObj')
+load('./STModel-Calibration/scripts/scale_info.Robj')
 
-names(spaceClim_SDM)[8] <- "annual_pp"
-names(spaceClim_SDM)[5] <- "mean_temperatre_wettest_quarter"
+# load data reshaped
+load('./STModel-Data/out_files/transitions_r1.rdata')
 
-load('../data/Multinom_6vars_version_b.rObj')
-load('../data/RandomForest_7vars.rObj')
+# Load SDM grid
+SDMClimate_grid <- read.csv('./STModel-Data/out_files/SDMClimate_grid.csv')
 
-pred_multinom <- predict(SDM1.b,new=spaceClim_SDM[,-c(1:2)],"prob")
-pred_RF <- predict(SDM2,new=spaceClim_SDM[,-c(1:2)],"prob")
+# Remove climatic spaces outside of the SDM calibration
+selectedVars = c("annual_mean_temp", "tot_annual_pp", "mean_diurnal_range", "pp_warmest_quarter", "pp_wettest_period", "mean_temp_wettest_quarter", "mean_temp_driest_quarter")
 
-pred <- pred_multinom
+# Get only variable selected
+sdm_calib <- stateData[,selectedVars]
+sdm_grid  <- SDMClimate_grid[,c("lat","lon",selectedVars)]
 
-pred <- cbind(pred[,c(1,2)],pred)
-pred <- melt(pred,id=c("lon","lat"))
-names(pred)<-c("lon","lat","state","prob")
-pred$prob <- cut(pred$prob,11)
-pred$prob <- factor(pred$prob,levels=rev(levels(pred$prob)))
+# Removed cells with temp>10°C
+sdm_grid  <- subset(sdm_grid,annual_mean_temp<=10)
+
+# Removed cells outside of the climatic space
+for (i in 1:(ncol(sdm_calib))){
+    rg <- range(sdm_calib[,i])
+    sdm_grid <- sdm_grid[which(sdm_grid[,i+2]>=rg[1] & sdm_grid[,i+2]<=rg[2]),]
+}
+
+# Scales clim variables
+for (i in 3:(ncol(sdm_grid))){
+    col <- names(sdm_grid)[i]
+    sdm_grid[,i] <- (sdm_grid[,i]-vars.means[col])/vars.sd[col]
+}
+
+
+# Grid prediction and Reshaping
+pred_multinom <- predict(SDM1,new=sdm_grid,"prob")
+pred_multinom <- data.frame(sdm_grid[,c("lat","lon")],pred_multinom,mod=rep("MN",rep=nrow(pred_multinom)))
+pred_RF <- predict(SDM2,new=sdm_grid,"prob")
+pred_RF <- data.frame(sdm_grid[,c("lat","lon")],pred_RF,mod=rep("RF",rep=nrow(pred_RF)))
+pred <- rbind(pred_RF,pred_multinom)
+
+df.pred <- melt(pred,c("lat","lon","mod"),value.name = "prob",variable.name = "state")
+df.pred$mod <- as.factor(df.pred$mod)
+df.pred$prob <- cut(df.pred$prob,breaks=11)
+df.pred$prob <-factor(df.pred$prob ,levels=rev(levels(df.pred$prob)))
+
+# Save Projs
+save(pred,file="./data/sdm_proj.rdata")
+
+
+# Crop lakes and countries on the area
+lakes <- readOGR(dsn="./STModel-Data/out_files/shapefiles/",layer="lakes_stm_area")
+countries <- readOGR(dsn="./STModel-Data/out_files/shapefiles/",layer="countries_stm_area")
+
+ext <- extent(c(range(sdm_grid$lon),range(sdm_grid$lat)))
+
+lakes <- crop(lakes,ext)
+countries <- crop(countries,ext)
+
+df.countries <- fortify(countries)
+df.lakes <- fortify(lakes)
+
+
+###########################################################################
+## Maps
+
+## set ggplot2 theme
+theme_set(theme_grey(base_size=14))
 
 cols <- brewer.pal(11,"Spectral")
 
-map_pred <- ggplot(pred) +
-geom_polygon(data = regions_usa, aes(x = long, y = lat, group = group),fill="grey80",colour="grey50",size=0.1) +
-geom_polygon(data = regions_can, aes(x = long, y = lat, group = group),fill="grey80",colour="grey50",size=0.1) +
-geom_raster(aes(lon,lat,fill=prob)) +
-facet_wrap(~state)+
-scale_fill_manual(values=cols,name="Class") +
-geom_polygon(data = subset(lakes_usa,hole==FALSE), aes(x = long, y = lat, group = group),fill="lightskyblue",colour="dodgerblue4",size=0.1) +
-geom_polygon(data = subset(lakes_can,hole==FALSE), aes(x = long, y = lat, group = group),fill="lightskyblue",colour="dodgerblue4",size=0.1) +
-scale_x_continuous(expand=c(0,0)) + scale_y_continuous(expand=c(0,0))+
-coord_equal() +
-xlab("Longitude") + ylab("Latitude")+
-theme(plot.title = element_text(face="bold"),
-    panel.background = element_rect(fill = "lightskyblue"),
-    panel.grid.minor = element_blank(),
-    panel.grid.major = element_line(color = "grey90",size=0.1),
-    strip.text.x = element_text(size = 12,face="bold" ,colour = "white"),strip.background = element_rect(colour="black", fill="black"))
+map <- ggplot(df.pred) +
+        geom_polygon(data = df.countries, aes(x = long, y = lat, group = group),fill="grey80",colour="grey50",size=0.1) +
+        geom_raster(aes(lon,lat,fill=prob)) +
+        facet_grid(state~mod)+
+        scale_fill_manual(values=cols,name="Probability") +
+        geom_polygon(data = subset(df.lakes,hole==FALSE),
+            aes(x = long, y = lat, group = group),fill="lightskyblue",
+            colour="dodgerblue4",size=0.1) +
+        scale_x_continuous(expand=c(0,0))+
+        scale_y_continuous(expand=c(0,0))+
+        coord_equal() +
+        xlab("Longitude") + ylab("Latitude")
 
-ggsave(map_pred,file="../figures/multinom_proj_SDM.jpg",width=15,height=12)
-
-###### ###### ###### ###### ###### ###### ######
-###### Climatic map
-
-map_var <- function(dat,var,pal,n){
-
-    datvar <- subset(dat,biovar == var)
-    datvar$val <- cut(datvar$val,n, dig.lab = 2)
-    datvar$val <- factor(datvar$val,levels=rev(levels(datvar$val)))
-
-    cols <- brewer.pal(n,pal)
-
-    ggplot(datvar) +
-    geom_polygon(data = regions_usa, aes(x = long, y = lat, group = group),fill="grey80",colour="grey50",size=0.1) +
-    geom_polygon(data = regions_can, aes(x = long, y = lat, group = group),fill="grey80",colour="grey50",size=0.1) +
-    geom_raster(aes(lon,lat,fill=val, order = val)) +
-    scale_fill_manual(values=cols,name="Class") +
-    geom_polygon(data = subset(lakes_usa,hole==FALSE), aes(x = long, y = lat, group = group),fill="lightskyblue",colour="dodgerblue4",size=0.1) +
-    geom_polygon(data = subset(lakes_can,hole==FALSE), aes(x = long, y = lat, group = group),fill="lightskyblue",colour="dodgerblue4",size=0.1) +
-    scale_x_continuous(expand=c(0,0)) + scale_y_continuous(expand=c(0,0))+
-    coord_equal() +
-    xlab("Longitude") + ylab("Latitude")+
-    theme(plot.title = element_text(face="bold"),
-        panel.background = element_rect(fill = "lightskyblue"),
-        panel.grid.minor = element_blank(),
-        panel.grid.major = element_line(color = "grey90",size=0.1),
-        strip.text.x = element_text(size = 12,face="bold" ,colour = "white"),strip.background = element_rect(colour="black", fill="black"))+
-    ggtitle(paste(var,"(Horizon 1970-2000)"))
-}
-
-res_SDMClimate_grid$biovar <- factor(res_SDMClimate_grid$biovar,labels=c(
-                    "Annual mean temperature (°C)",
-                    "Mean diurnal range (JD)",
-                    "Mean temperature of the wettest period (°C)",
-                    "Precipitation seasonality (mm)",
-                    "Precipitation of the warmest quarter (mm)",
-                    "Annual precipitation (mm)"))
+ggsave(map,file="./figures/proj_SDM.jpg",width=8,height=10)
 
 
- for(i in 1:nlevels(res_SDMClimate_grid$biovar)){
-    map_var(res_SDMClimate_grid,levels(res_SDMClimate_grid$biovar)[i],"Spectral",11)
-    ggsave(paste("../figures/var",i,"_1970-2000.jpg",sep=""),height=5,width=7)
- }
+map <- ggplot(subset(df.pred,mod=='RF')) +
+        geom_polygon(data = df.countries, aes(x = long, y = lat, group = group),fill="grey80",colour="grey50",size=0.1) +
+        geom_raster(aes(lon,lat,fill=prob)) +
+        facet_wrap(~state)+
+        scale_fill_manual(values=cols,name="Probability") +
+        geom_polygon(data = subset(df.lakes,hole==FALSE),
+            aes(x = long, y = lat, group = group),fill="lightskyblue",
+            colour="dodgerblue4",size=0.1) +
+        scale_x_continuous(expand=c(0,0))+
+        scale_y_continuous(expand=c(0,0))+
+        coord_equal() +
+        xlab("Longitude") + ylab("Latitude")
 
-###### ###### ###### ###### ###### ###### ######
-###### Climatic map
+ggsave(map,file="./figures/RF_proj_SDM.jpg",width=15,height=10)
+
+map <- ggplot(subset(df.pred,mod=='MN')) +
+        geom_polygon(data = df.countries, aes(x = long, y = lat, group = group),fill="grey80",colour="grey50",size=0.1) +
+        geom_raster(aes(lon,lat,fill=prob)) +
+        facet_wrap(~state)+
+        scale_fill_manual(values=cols,name="Probability") +
+        geom_polygon(data = subset(df.lakes,hole==FALSE),
+            aes(x = long, y = lat, group = group),fill="lightskyblue",
+            colour="dodgerblue4",size=0.1) +
+        scale_x_continuous(expand=c(0,0))+
+        scale_y_continuous(expand=c(0,0))+
+        coord_equal() +
+        xlab("Longitude") + ylab("Latitude")
+
+ggsave(map,file="./figures/MN_proj_SDM.jpg",width=15,height=10)
+
+################################################
+###### Explore SDM response to climate
 
 require(dplyr)
 
@@ -158,21 +172,10 @@ for (i in 1:ncol(desc_val)){
 ggdata <- melt(do.call(rbind,out_ls),id=c("model","var_test","value_var_test"),value.name="probability",variable.name="state")
 
 require(ggplot2)
-
-theme_set(theme_grey(base_size=14))
-
-x11()
 ggplot(subset(ggdata,model=="MN"),aes(x=value_var_test,y=probability,colour=state)) + geom_line() + facet_wrap(~var_test,scales="free_x") + xlab("Var tested") + ylab("Probability")
 ggsave(file="../figures/MN_oneVar_test.jpg",width=12,height=8)
 
 ggplot(subset(ggdata,model=="RF"),aes(x=value_var_test,y=probability,colour=state)) + geom_line() + facet_wrap(~var_test,scales="free_x") + xlab("Var tested") + ylab("Probability")
 ggsave(file="../figures/RF_oneVar_test.jpg",width=12,height=8)
-
-
-###### ###### ###### ###### ###### ###### ######
-
-
-plot_dat <- read.csv2('../../STModel-Data/out_files/treeData.csv')
-str(plot_dat)
 
 
